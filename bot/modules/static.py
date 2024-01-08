a6b235f3b4376f9cf319235b6751d15f4c9bff7b
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2024-01-09 04:38:41 +0700

    Update static.py

--- bot/modules/static.py
+++ bot/modules/static.py
@@ -1,13 +1,5 @@
 from bot.config import Telegram
 
-WelcomeText = \
-"""
-Hi **%(first_name)s**, send me a message or designate me as an admin in your groups. I'll promptly respond by attaching my reaction to the received message.
-
-- /start to get this message.
-- /emojis to get supported emojis. 
-"""
-
 SupportedEmojisText = \
 """
 **I currently support following emojis:**
